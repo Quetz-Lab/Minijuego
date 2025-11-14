674758214cd60bbb8db56b112c3ee1bc521dbfbd
darwok <darwok.bit@outlook.com>
2025-11-14 13:33:47 -0600

    Delete nodes P1
    Created first methods to delete "skills"

--- Minijuego/Minijuego.cpp
+++ Minijuego/Minijuego.cpp
@@ -3,18 +3,104 @@
 
 #include <iostream>
 #include "SFML/Graphics.hpp"
+#include <string>
+
+using namespace std;
+
+struct Node
+{
+    string data;   // Nombre de la habilidad
+    Node* next;
+
+    Node(const string& data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class SkillList
+{
+public:
+    Node* head;
+    SkillList():head(nullptr){}
+    ~SkillList()
+    {
+        Clear();
+    }
+};
+
+// Add skill at the end of the list, if there are no other skills, then it becomes the head.
+void AddSkill(const string& data)
+{
+    Node* newNode = new Node(data);
+
+    if (head == nullptr) head = newNode;
+    else
+    {
+        Node* temp = head;
+        while (temp->next != nullptr) temp = temp->next;
+        temp->next = newNode;
+    }
+    cout << "Skill added!!" << data << endl;
+}
+
+// Delete head, first hability (only if it exists, if not, debug an error message.
+void DeleteHead()
+{
+    if (!head)
+    {
+        cout << "List is empty, can't remove first hability." << endl;
+        return;
+    }
+
+    Node* newHead = head->next;
+    cout << "Removing skill, index 0" << head->data << endl;
+    delete head;
+    head = newHead;
+}
+
+// Delete skill by index, same as before, if it does not exists, it shows a message in console
+bool DeleteSkillAt(int index)
+{
+    if (index < 0 || !head)
+    {
+        cout << "Index invalid, empty list of skills." << endl;
+        return false;
+    }
+
+    // Case of index 0 -> Head, the we call DeleteHead();
+    if (index == 0)
+    {
+        DeleteHead();
+        return true;
+    }
+
+    Node* temp = head;
+    int currentIndex = 0;
+
+    while (temp != nullptr || temp->next == nullptr)
+    {
+        temp = temp->next;
+        currentIndex++;
+    }
+
+    if (temp == nullptr || temp->next == nullptr)
+    {
+        cout << "Index out of range. No skill was removed." << endl;
+        return false;
+    }
+
+    Node* nodeToDelete = temp->next;
+    cout << "Removing skill, index"<< index << nodeToDelete->data << endl;
+    temp->next = nodeToDelete->next;
+    delete nodeToDelete;
+    return true;
+}
+
+
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
